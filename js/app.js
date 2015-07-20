b012a180ce3c14e03783999a0652dfac386fa31d
Chad Ackerman <chad.ackerman@nitelifecam.comgit config --global user.email ackermcc@gmail.com>
2015-07-19 22:52:34 -0700

    still a scrolling issue...

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 $(window).load(function(){
 
+	var height = window.innerHeight;
+    // $('iframe').css('height', height);
+
+    var isOldIE = (navigator.userAgent.indexOf("MSIE") !== -1); // Detect IE10 and below
+
 	$('iframe').iFrameResize({
 		log: 'true',
-		heightCalculationMethod: 'max'
+		heightCalculationMethod: isOldIE ? 'max' : 'lowestElement',
+		minHeight: height,
+		scrolling: 'true'
 	});
 
 });
